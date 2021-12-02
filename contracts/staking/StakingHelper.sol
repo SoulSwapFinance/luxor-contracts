d0db5dd69ec95e30261417ebc10bb71a22a3fa01
Buns <bunsthedev@gmail.com>
2021-12-02 01:28:07 -0600

    update: ZapIn, Treasury, BondDepository, Calc

--- contracts/staking/StakingHelper.sol
+++ contracts/staking/StakingHelper.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
-
 
 interface IERC20 {
     function decimals() external view returns (uint8);
